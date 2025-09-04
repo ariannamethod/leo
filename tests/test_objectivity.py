7fb0e92bb741a595fc3ec407dc605ab77f8aabb5
Arianna Method <theariannamethod@gmail.com>
2025-09-05 00:35:56 +0300

    test filtering of ignored tokens

---
+++ tests/test_objectivity.py
@@ -0,0 +1,34 @@
+import objectivity
+from objectivity import Objectivity
+
+
+def test_ignored_tokens_not_in_queries(monkeypatch):
+    sent_queries = []
+
+    def fake_ddg_json(query):
+        sent_queries.append(query)
+        return []
+
+    monkeypatch.setattr(objectivity, "_ddg_json", fake_ddg_json)
+
+    obj = Objectivity()
+    obj.context_window("talk about banana", ["banana"])
+
+    assert sent_queries == []
+
+
+def test_mixed_queries_filter(monkeypatch):
+    sent_queries = []
+
+    def fake_ddg_json(query):
+        sent_queries.append(query)
+        return []
+
+    monkeypatch.setattr(objectivity, "_ddg_json", fake_ddg_json)
+
+    obj = Objectivity()
+    obj.context_window("How to make salad", ["responding"])
+
+    assert sent_queries  # other queries should remain
+    assert all("responding" not in q for q in sent_queries)
+
